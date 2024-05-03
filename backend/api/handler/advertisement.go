9b177c6610fb693f80524a6c6957835063ac08a5
artificial00 <154523621+artificial00@users.noreply.github.com>
2024-05-03 20:57:47 +0300

    IQJ-19 Добавлена функция на получение всех объявлений, хранящихся в бд

--- backend/api/handler/advertisement.go
+++ backend/api/handler/advertisement.go
@@ -127,3 +127,32 @@ func (h *Handler) HandleUpdateAdvertisements(c *gin.Context) {
 		c.JSON(http.StatusForbidden, "There are not enough rights for this action")
 	}
 }
+
+// Функция для получения всех объявлений, имеющихся в бд на данный момент.
+// Извлекает из запроса параметр all_ads, который должен быть равен 1 для работы.
+// Используется функция GetAllAds, которая получает срез всех объявлений в бд.
+// Использование с GET: /news?all_ads=1
+func (h *Handler) HandleGetAllAds(c *gin.Context) {
+  all_adsStr := c.Query("all_ads")
+
+  all_ads, err := strconv.Atoi(all_adsStr)
+  if err != nil{
+    c.String(http.StatusBadRequest, err.Error())
+    fmt.Println("HandleGetAllAds:", err)
+    return
+  }
+
+  if all_ads != 1 {
+    c.JSON(http.StatusBadRequest, "All_ads != 1")
+    fmt.Println("HandleGetAllAds: all_ads != 1")
+    return
+  }
+
+  allads, err := database.Database.News.GetAllAds()
+  if err != nil {
+    c.JSON(http.StatusInternalServerError, err.Error())
+    fmt.Println("HandleGetAllNews:", err)
+    return
+  }
+  c.JSON(http.StatusOK, allads)
+}
