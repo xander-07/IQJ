2e8ff35f76ee0f1a238670262a24e71e97d8fbfa
Varya <barbarakushaeva@gmail.com>
2024-05-03 19:14:35 +0300

    IQJ-10 Убрана кнопка arrow, исправлен размер виджета, цвета, текст(кнопка админ)

--- lib/features/news/admin/admin_button.dart
+++ lib/features/news/admin/admin_button.dart
@@ -6,82 +6,11 @@ import 'package:iqj/features/news/admin/special_news.dart';
 import 'package:iqj/features/news/presentation/bloc/news_bloc.dart';
 import 'package:iqj/features/news/presentation/screens/search/body_for_date/body.dart';
 
-// не удаляйте плиз :) нужно потом будет
-
-// void admin_button(BuildContext context,NewsBloc newsBloc) {
-//   TextEditingController _titleController = TextEditingController();
-//   TextEditingController _contentController = TextEditingController();
-//   // final _newsBloc = newsBloc.BlocProvider(
-//   //   create: (context) => NewsBloc(),
-//   //   child: Container(),
-//   // );
-//   GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
-
-//   showDialog(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return AlertDialog(
-//           title: Text('Add News'),
-//           content: Column(
-//             mainAxisSize: MainAxisSize.min,
-//             children: <Widget>[
-//               TextField(
-//                 controller: _titleController,
-//                 decoration: InputDecoration(hintText: 'Title'),
-//               ),
-//               TextField(
-//                 controller: _contentController,
-//                 decoration: InputDecoration(hintText: 'Content'),
-//               ),
-//             ],
-//           ),
-//           actions: <Widget>[
-//             TextButton(
-//               child: Text('Cancel'),
-//               onPressed: () {
-//                 Navigator.of(context).pop();
-//               },
-//             ),
-//             TextButton(
-//               child: Text('Add'),
-//               onPressed: () {
-//                 // NewsModel news = NewsModel(
-//                 //   title: _titleController.text,
-//                 //   content: _contentController.text,
-//                 // );
-//                 // newsBloc.add(AddNews(news));
-//                 final NewsSmall news = NewsSmall(
-//                   thumbnail: "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
-//                 title: _titleController.text,
-//                 date: "13.12.2005 :)",
-//                 description: _contentController.text,
-//                 );
-//                 //newsbloc.add(AddNewsEvent(news: news));
-//                 newsBloc.add(AddNewsEvent(news: news));
-//                 Navigator.of(context).pop();
-//                 Future.delayed(Duration.zero, () {
-//                   _refreshIndicatorKey.currentState?.show();
-//                 });
-//               },
-//             ),
-//           ],
-//         );
-//       },
-//     );
-// }
-
 Future<void> admin_button(BuildContext context) async {
   final AlertDialog alert = AlertDialog(
-    title: Row(
+    title: const Row(
       children: [
-        IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          icon: const Icon(Icons.arrow_back),
-        ),
-        const Padding(padding: EdgeInsets.only(right: 6)),
-        const Flexible(
+        Flexible(
           child: Text(
             "Создать новость",
             overflow: TextOverflow.clip,
@@ -95,105 +24,104 @@ Future<void> admin_button(BuildContext context) async {
     ),
     backgroundColor: Theme.of(context).colorScheme.background,
     surfaceTintColor: Colors.white,
-    content: two_button_add_news(context),
+    content: ConstrainedBox(
+      constraints: BoxConstraints(),
+      child: two_button_add_news(context),
+    ),
   );
 
   showDialog(
     context: context,
     builder: (BuildContext context) {
-      return WillPopScope(
-        onWillPop: () async {
-          Navigator.of(context).pop();
-          return true;
-        },
-        child: alert,
-      );
+      return alert;
     },
   );
 }
 
+
+
 Widget two_button_add_news(BuildContext context) {
-  return Container(
-    height: 144,
-    width: 325,
-    decoration: BoxDecoration(
-      color: Colors.transparent,
-      border: Border(
-        top: BorderSide(
-          color: Theme.of(context).colorScheme.surfaceVariant,
+  return Wrap(
+    children: [
+      Container(
+        decoration: const BoxDecoration(
+          color: Colors.transparent,
+          border: Border(
+            top: BorderSide(
+              color: Color(0xFF48483D),
+            ),
+          ),
+        ),
+        child: Column(
+          children: [
+            const SizedBox(height: 6), 
+            SizedBox(
+              height: 65,
+              width: 325,
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Material(
+                  color: const Color.fromRGBO(239, 172, 0, 1),
+                  shape: const StadiumBorder(),
+                  elevation: 5.0,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SpecialNews(),
+                        ),
+                      );
+                    },
+                    child: const Center(
+                      child: Text(
+                        'Важную',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 65,
+              width: 325,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 15, left: 8, right: 8),
+                child: Material(
+                  color: const Color.fromRGBO(239, 172, 0, 1),
+                  shape: const StadiumBorder(),
+                  elevation: 5.0,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const GeneralNews()),
+                      );
+                    },
+                    child: const Center(
+                      child: Text(
+                        'Общую',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
-    ),
-    child: Column(
-      children: [
-        const Padding(
-          padding: EdgeInsets.only(top: 12),
-        ),
-        SizedBox(
-          height: 65,
-          width: 277,
-          child: Padding(
-            padding:
-                const EdgeInsets.only(top: 8, left: 8, right: 8, bottom: 8),
-            child: Material(
-              color: const Color.fromRGBO(239, 172, 0, 1),
-              shape: const StadiumBorder(),
-              elevation: 5.0,
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const SpecialNews(),
-                    ),
-                  );
-                },
-                child: const Center(
-                  //padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                  child: Text(
-                    'Важную',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 65,
-          width: 277,
-          child: Padding(
-            padding: const EdgeInsets.only(top: 15, left: 8, right: 8),
-            child: Material(
-              color: const Color.fromRGBO(239, 172, 0, 1),
-              shape: const StadiumBorder(),
-              elevation: 5.0,
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const GeneralNews()),
-                  );
-                },
-                child: const Center(
-                  //padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                  child: Text(
-                    'Общую',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
-    ),
+    ],
   );
 }
+
