4085d930750ef7dc6f0653947fdbb5e3864be86c
daniil <hynslei@ya.ru>
2024-05-05 01:45:19 +0300

    Реализация флага "для студентов"

--- backend/database/news.go
+++ backend/database/news.go
@@ -17,6 +17,7 @@ type News struct {
 	Content         string   `json:"content,omitempty"` // Содержание новости (текст)
 	ImageLinks      []string `json:"image_link"`        // Ссылки на изображения
 	Tags            []string `json:"tags"`              // Теги
+	IsForStudents   bool     `json:"is_for_students"`   // Для кого предназначена новость (для студентов -> true)
 	PublicationTime string   `json:"publication_time"`  // Время публикации новости в формате "DD.MM.YYYY"
 }
 
@@ -50,13 +51,13 @@ func (nt *NewsTable) Add(n *News) error {
 	n.PublicationTime = formattedDate.Format("2006-01-02 15:04:05")
 
 	err = nt.qm.makeInsert(nt.db,
-		`INSERT INTO news (header, link, news_text, image_links, tags, publication_time)
-			SELECT $1, $2, $3, $4, $5, $6
+		`INSERT INTO news (header, link, news_text, image_links, tags, is_for_students,publication_time)
+			SELECT $1, $2, $3, $4, $5, $6, $7
 			WHERE NOT EXISTS (
-				SELECT 1 FROM news WHERE header = $1 AND publication_time = $6
+				SELECT 1 FROM news WHERE header = $1 AND publication_time = $7
 			)
 		`,
-		n.Header, n.Link, n.Content, pq.Array(n.ImageLinks), pq.Array(n.Tags), n.PublicationTime)
+		n.Header, n.Link, n.Content, pq.Array(n.ImageLinks), pq.Array(n.Tags), n.IsForStudents, n.PublicationTime)
 
 	if err != nil {
 		return fmt.Errorf("News.Add: %v", err)
@@ -82,7 +83,7 @@ func (nt *NewsTable) GetById(n *News) (*News, error) {
 	}
 
 	rows, err := nt.qm.makeSelect(nt.db,
-		"SELECT header, link, news_text, image_links, tags, publication_time FROM news WHERE news_id = $1",
+		"SELECT header, link, news_text, image_links, tags, is_for_student, publication_time FROM news WHERE news_id = $1",
 		n.Id,
 	)
 	defer rows.Close()
@@ -91,11 +92,11 @@ func (nt *NewsTable) GetById(n *News) (*News, error) {
 		return nil, fmt.Errorf("News.GetById: %v", err)
 	}
 
-	if rows.Next() {
-		rows.Scan(&n.Header, &n.Link, &n.Content, pq.Array(&n.ImageLinks), pq.Array(&n.Tags), &n.PublicationTime)
-	} else {
+	if !rows.Next() {
 		return nil, errors.New("News.GetById: could not find any News with provided *News.Id")
 	}
+
+	rows.Scan(&n.Header, &n.Link, &n.Content, pq.Array(&n.ImageLinks), pq.Array(&n.Tags), &n.IsForStudents, &n.PublicationTime)
 
 	return n, nil
 }
@@ -103,7 +104,7 @@ func (nt *NewsTable) GetById(n *News) (*News, error) {
 func (nt *NewsTable) GetAll() (*[]News, error) {
 
 	rows, err := nt.qm.makeSelect(nt.db,
-		"SELECT news_id, header, link, news_text, image_links, tags, publication_time FROM news",
+		"SELECT news_id, header, link, news_text, image_links, tags, is_for_students,publication_time FROM news",
 	)
 	defer rows.Close()
 
@@ -114,7 +115,7 @@ func (nt *NewsTable) GetAll() (*[]News, error) {
 	var n News
 	var nArr []News
 	for rows.Next() {
-		rows.Scan(&n.Id, &n.Header, &n.Link, &n.Content, pq.Array(&n.ImageLinks), pq.Array(&n.Tags), &n.PublicationTime)
+		rows.Scan(&n.Id, &n.Header, &n.Link, &n.Content, pq.Array(&n.ImageLinks), pq.Array(&n.Tags), &n.IsForStudents, &n.PublicationTime)
 		nArr = append(nArr, n)
 	}
 
@@ -130,6 +131,33 @@ func (nt *NewsTable) GetAll() (*[]News, error) {
 func (nt *NewsTable) GetLatestBlocks(count, offset int) (*[]News, error) {
 	rows, err := nt.qm.makeSelect(nt.db,
 		"SELECT news_id, header, link, image_links, publication_time FROM news ORDER BY publication_time DESC LIMIT $1 OFFSET $2",
+		count, offset,
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("News.GetLatest: %v", err)
+	}
+
+	var resultNewsArr []News
+	var resultNews News
+
+	for rows.Next() {
+		rows.Scan(&resultNews.Id, &resultNews.Header, &resultNews.Link, pq.Array(&resultNews.ImageLinks), &resultNews.PublicationTime)
+		resultNewsArr = append(resultNewsArr, resultNews)
+	}
+
+	return &resultNewsArr, nil
+}
+
+// GetLatestBlocks возвращает указанное количество последних новостных блоков.
+// Принимает количество блоков и смещение.
+// Возвращает срез News и nil при успешном запросе.
+//
+// Прим:
+// blocks, err := ...GetLatestBlocks(10, 0) // Получить 10 последних новостных блоков
+func (nt *NewsTable) GetLatestBlocksForStudents(count, offset int) (*[]News, error) {
+	rows, err := nt.qm.makeSelect(nt.db,
+		"SELECT news_id, header, link, image_links, publication_time FROM news ORDER BY publication_time DESC LIMIT $1 OFFSET $2 WHERE is_for_students = true",
 		count, offset,
 	)
 
