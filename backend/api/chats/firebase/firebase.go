2c12f0cb40bcec69f7baa5dbefba90f43cadb18d
Denis <ddm22026@gmail.com>
2024-05-06 17:23:28 +0300

    Изменения в чате

--- backend/api/chats/firebase/firebase.go
+++ backend/api/chats/firebase/firebase.go
@@ -4,21 +4,45 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+
+	"cloud.google.com/go/firestore"
 	"google.golang.org/api/option"
 
 	firebase "firebase.google.com/go"
 )
 
 func main() {
+	_, err := createClient()
+	if err != nil {
+		log.Printf("error client %v", err)
+		return
+	}
 
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello, World!")
+	})
+	r.GET("/api/health", HealthCheckHandler())
+	// r.Run("127.0.0.1:5050")
+}
+
+func HealthCheckHandler() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"ok": true})
+	}
+}
+
+func createClient() (*firestore.Client, error) {
 	ctx := context.Background()
 	//conf := &firebase.Config{ProjectID: "iqj-uniapp"}
 	opt := option.WithCredentialsFile("C:/Users/ddm22/Downloads/iqj-uniapp-firebase-adminsdk-78svx-69c52bf7c6.json")
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		fmt.Errorf("error initializing app: %v", err)
-		return
+		return nil, err
 	}
 	// app, err := firebase.NewApp(ctx, conf)
 
@@ -33,19 +57,20 @@ func main() {
 		log.Fatal(err)
 	}
 
-	ref := client.Collection("some_exp").NewDoc()
-	result, err := ref.Set(ctx, map[string]interface{}{
-		"adress":  "a random human",
-		"message": "Hi",
-	})
+	return client, nil
 
-	if err != nil {
-		log.Printf("error %v", err)
-		log.Fatal(err)
-	}
+	// ref := client.Collection("some_exp").NewDoc()
+	// result, err := ref.Set(ctx, map[string]interface{}{
+	// 	"adress":  "a random human",
+	// 	"message": "Hi",
+	// })
 
-	log.Printf("result is %v", result)
+	// if err != nil {
+	// 	log.Printf("error %v", err)
+	// 	log.Fatal(err)
+	// }
 
+	// log.Printf("result is %v", result)
 }
 
 // package main
---
+++ backend/api/chats/firebase/health_check.go
@@ -0,0 +1,13 @@
+package firebase
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HealthCheckHandler() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"ok": true})
+	}
+}
