3166b14723e2cc58eba3170ced31daf25e26c844
Denis <ddm22026@gmail.com>
2024-05-08 21:55:53 +0300

    IQJ-28 исправление структуры сообщения для firebase(go)

--- backend/api/firebase/firebase_mes.go
+++ backend/api/firebase/firebase_mes.go
@@ -1,49 +1,49 @@
 package firebase
 
 import (
-	"fmt"
-	"iqj/config"
-	"iqj/firebase"
+	"time"
+
+	//"iqj/firebase"
 	"log"
 
 	"cloud.google.com/go/firestore"
-	firebase "firebase.google.com/go/v4"
-	"firebase.google.com/go/v4/auth"
-	"github.com/googleapis/enterprise-certificate-proxy/client"
 	"golang.org/x/net/context"
-	"google.golang.org/api/option"
 )
 
-type Message struct{
-	ID int 
-	Text string
-	Sender auth.UserInfo
-} 
+type Message struct {
+	Message     string    `json:"message"`
+	ReceiverId  string    `json:"receiverId"`
+	SenderEmail string    `json:"senderEmail"`
+	SenderId    string    `json:"senderId"`
+	Timestamp   time.Time `json:"timestamp"`
+}
 
 const (
 	projectID = "iqj-uniapp"
 )
 
-func giveMessfirebase(mess Message) (*Message,error){
+func giveMessfirebase(mess Message) (*Message, error) {
 	ctx := context.Background()
-	client,err := firestore.NewClient(ctx,projectID)
-	if (err!=nil){
-		log.Fatal("error client %v",err)
-		return nil,err
+	client, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatal("error client %v", err)
+		return nil, err
 	}
 
 	defer client.Close()
 
-	_,_,err = client.Collection("posts").Add(ctx,map[string]interface{}{
-		'ID': mess.ID,
-		'Text': mess.Text,
-		'Sender': mess.Sender,
+	_, _, err = client.Collection("posts").Add(ctx, map[string]interface{}{
+		"Message":     mess.Message,
+		"ReceiverId":  mess.ReceiverId,
+		"SenderEmail": mess.SenderEmail,
+		"SenderId ":   mess.SenderId,
+		"Timestamp":   mess.Timestamp,
 	})
 
-	if err!=nil{
-		log.Fatal("firestor client error: %v",err)
-		return nil,err
+	if err != nil {
+		log.Fatal("firestor client error: %v", err)
+		return nil, err
 	}
 
-	return mess,nil
+	return mess, nil
 }
