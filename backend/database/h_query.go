3bd8b9f0bbbf237131a5e837b54b3ea416ab1d0b
daniil <hynslei@ya.ru>
2024-05-10 23:47:48 +0300

    Добавление дефолтных значений в classes
    для избежания ошибки сканирования null

--- backend/database/h_query.go
+++ backend/database/h_query.go
@@ -70,16 +70,16 @@ CREATE TABLE IF NOT EXISTS student_groups (
 	createTableClasses = `
 	CREATE TABLE IF NOT EXISTS classes (
     class_id SERIAL PRIMARY KEY,
-    class_group_ids INT[],
-    class_group_names VARCHAR(11)[] NOT NULL,
-    class_teacher_id INT,
+    class_group_ids INT[] DEFAULT '{0}',
+    class_group_names VARCHAR(11)[] DEFAULT '{"default"}' NOT NULL,
+    class_teacher_id INT DEFAULT 0,
     class_teacher_name VARCHAR(255),
-    count INT NOT NULL,
-    weekday INT NOT NULL,
-    week INT NOT NULL,
+    count INT DEFAULT 0 NOT NULL,
+    weekday INT DEFAULT 0 NOT NULL,
+    week INT DEFAULT 0 NOT NULL,
     class_name TEXT,
     class_type TEXT,
-    class_location TEXT
+    class_location TEXT DEFAULT ''
 );
 
 CREATE INDEX IF NOT EXISTS idx_class_id  ON classes (class_id);
