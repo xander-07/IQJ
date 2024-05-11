ad1250bd147a6c5ef724f978561dc77655f7109a
Gewinn2 <daniilpostnickov@yandex.ru>
2024-05-11 10:09:09 +0300

    Отключен кэш

--- backend/api/handler/schedule.go
+++ backend/api/handler/schedule.go
@@ -28,66 +28,67 @@ func (h *Handler) Lessons(c *gin.Context) {
 	filteredLessons := &[]database.Class{}
 
 	//Проверяем, есть ли заданное значение в кэше
-	item := cache.Get(value)
-	if item != nil { //Если есть, возвращаем его
-		c.JSON(http.StatusOK, item)
-	} else {
-		var err error
+	//item := cache.Get(value)
+	//if item != nil { //Если есть, возвращаем его
+	//	c.JSON(http.StatusOK, item)
+	//} else {
+	var err error
 
-		//Switch по заданному критерию
-		switch criterion {
-		//По группе
-		case "group":
-			//Создание группы
-			group := &database.StudentGroup{}
-			//Заданное value присваивается полю имени группы
-			group.Name = value
-			//Поиск ID группы по имени
-			group, err := database.Database.StudentGroup.GetIdByName(group)
-			if err != nil {
-				c.String(http.StatusBadRequest, err.Error())
-			}
-			//Получаем список пар по группе из БД
-			filteredLessons1, err := database.Database.StudentGroup.GetClasses(group)
-			if err != nil {
-				c.String(http.StatusBadRequest, err.Error())
-			}
-			filteredLessons = &filteredLessons1
-			//По преподавателю
-		case "tutor":
-			//TODO: Раскомментировать, когда будет готов запрос
-			//Заданное value присваивается полю ФИО препода пары
-			//lesson.TeacherName = value
-			//Получаем список пар по преподу		
-			/*filteredLessons, err = database.Database.Class.GetForWeekByTeacher(lesson)
-			if err != nil {
-				c.String(http.StatusBadRequest, err.Error())
-			}*/
-			return
-
-			//По аудитории
-		case "classroom":
-			filteredLessons2 := []database.Class{}
-			//Заданное value присваивается полю аудитории пары
-			lesson.Location = value
-			//Получаем список пар по аудитории
-			classes, err := database.Database.Class.GetByLocation(lesson)
-			if err != nil {
-				c.String(http.StatusBadRequest, err.Error())
-			}
-			//Проходим по полученным парам и закидываем в список
-			for _, class := range *classes {
-				filteredLessons2 = append(filteredLessons2, class)
-			}
-			filteredLessons = &filteredLessons2
-			//При неверном критерии вернет BadRequest
-		default:
+	//Switch по заданному критерию
+	switch criterion {
+	//По группе
+	case "group":
+		//Создание группы
+		group := &database.StudentGroup{}
+		//Заданное value присваивается полю имени группы
+		group.Name = value
+		//Поиск ID группы по имени
+		group, err := database.Database.StudentGroup.GetIdByName(group)
+		if err != nil {
+			c.String(http.StatusBadRequest, err.Error())
+		}
+		//Получаем список пар по группе из БД
+		filteredLessons1, err := database.Database.StudentGroup.GetClasses(group)
+		if err != nil {
 			c.String(http.StatusBadRequest, err.Error())
 			return
 		}
+		filteredLessons = &filteredLessons1
+		//По преподавателю
+	case "tutor":
+		//Заданное value присваивается полю ФИО препода пары
+		lesson.TeacherName = value
+		//Получаем список пар по преподу
+		filteredLessons, err = database.Database.Class.GetByTeacher(lesson)
+		if err != nil {
+			c.String(http.StatusBadRequest, err.Error())
+			return
+		}
+
+		//По аудитории
+	case "classroom":
+		filteredLessons2 := []database.Class{}
+		//Заданное value присваивается полю аудитории пары
+		lesson.Location = value
+		//Получаем список пар по аудитории
+		classes, err := database.Database.Class.GetByLocation(lesson)
+		if err != nil {
+			c.String(http.StatusBadRequest, err.Error())
+			return
+		}
+		//Проходим по полученным парам и закидываем в список
+		for _, class := range *classes {
+			filteredLessons2 = append(filteredLessons2, class)
+		}
+		filteredLessons = &filteredLessons2
+		//При неверном критерии вернет BadRequest
+	default:
+		c.String(http.StatusBadRequest, err.Error())
+		return
 	}
+	//}
 	//Добавляем в кэш значение
-	cache.Set(value, filteredLessons)
+	//cache.Set(value, filteredLessons)
 	//Возвращаем StatusOK и пары по заданным значениям
 	c.JSON(http.StatusOK, filteredLessons)
 }
