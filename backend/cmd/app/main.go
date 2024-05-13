b17a8c94b2a0dcce649e34bd0ed6979734b38c5d
Denis <ddm22026@gmail.com>
2024-05-13 10:03:50 +0300

    исправление ошибки

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -29,11 +29,8 @@ func main() {
 	go excel_parser.Parse()
 
 	// Запускает сервер на порту и "слушает" запросы.
-	// if err := handlers.InitRoutes().Run(":8443"); err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	if err := handlers.InitRoutes().Run(":5050"); err != nil {
+	if err := handlers.InitRoutes().Run(":8443"); err != nil {
 		log.Fatal(err)
 	}
+
 }
