ecb88d1e9778c41893cb1f619b468d97923151c5
Gewinn2 <daniilpostnickov@yandex.ru>
2024-05-15 15:48:59 +0300

    Добавлены методы PUT и DELETE в CORS

--- backend/pkg/middleware/CORS.go
+++ backend/pkg/middleware/CORS.go
@@ -10,7 +10,7 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		origin := c.GetHeader("Origin")
 		c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
